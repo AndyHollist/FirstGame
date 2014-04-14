7244cfb355d1bf5d7a7f710732faa0357764b30e
Harrison Chen <harrisonxchen@gmail.com>
2014-04-14 14:21:03 -0700

    HARRISON IS AWESOME

---
+++ first_game-android/gen/com/AndyHollist/first_game/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.AndyHollist.first_game;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ first_game/src/com/AndyHollist/first_game/Paddle.java
@@ -0,0 +1,15 @@
+package com.AndyHollist.first_game;
+
+public class Paddle {
+	
+	double x_pos;
+	double y_pos;
+	double size;
+	
+	
+	
+	Paddle() {
+		
+	}
+
+}
