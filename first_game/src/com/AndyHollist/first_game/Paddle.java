e401a91075793b4651681c5b931f7ffde2fef8d6
Harrison Chen <harrisonxchen@gmail.com>
2014-04-14 14:28:04 -0700

    HArrison chagned

--- first_game/src/com/AndyHollist/first_game/Paddle.java
+++ first_game/src/com/AndyHollist/first_game/Paddle.java
@@ -1,12 +1,14 @@
 package com.AndyHollist.first_game;
 
+import com.badlogic.gdx.graphics.Texture;
+
 public class Paddle {
 	
 	double x_pos;
 	double y_pos;
 	double size;
 	
-	
+	Texture image;
 	
 	Paddle() {
 		
